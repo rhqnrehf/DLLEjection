da80a7130d1b4497030795d22770099df7a65ea9
xhfm77 <xhfm77@gmail.com>
2019-03-20 23:40:10 +0900

    All Process DllEjections

--- DllEjection/DllEjection.cpp
+++ DllEjection/DllEjection.cpp
@@ -1,21 +1,134 @@
-﻿// DllEjection.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include "pch.h"
 
-#include "pch.h"
-#include <iostream>
+using namespace std;
+vector<DWORD> FindProcessId(LPCTSTR szProcessName) {
+	vector<DWORD> vc;
+	DWORD dwPID = 0xffffffff;
+	HANDLE hSnapShot = INVALID_HANDLE_VALUE;
+	PROCESSENTRY32 pe;
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+	pe.dwSize = sizeof(PROCESSENTRY32);
+	hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
+
+	Process32First(hSnapShot, &pe);
+	do {
+		dwPID = pe.th32ProcessID;
+		vc.push_back(pe.th32ProcessID);
+	} while (Process32Next(hSnapShot, &pe));
+
+	CloseHandle(hSnapShot);
+
+	return vc;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+BOOL SetPrivilege(LPCTSTR lpszPrivilege, BOOL bEnablePrivilege) {
+	TOKEN_PRIVILEGES tp;
+	HANDLE hToken;
+	LUID luid;
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)) {
+		wcout << L"OpenProcessToken Error: " << GetLastError() << endl;
+		return FALSE;
+	}
+	if (!LookupPrivilegeValue(NULL,
+		lpszPrivilege,
+		&luid)) {
+		wcout << L"LookipPrivilegeValue Error: " << GetLastError() << endl;
+		return FALSE;
+	}
+
+	tp.PrivilegeCount = 1;
+	tp.Privileges[0].Luid = luid;
+
+	if (bEnablePrivilege) {
+		tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	}
+	else {
+		tp.Privileges[0].Attributes = 0;
+	}
+
+	if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), NULL, NULL)) {
+		wcout << L"AdjustTokenPrivileges Error: " << GetLastError() << endl;
+		return FALSE;
+	}
+	if (GetLastError() == ERROR_NOT_ALL_ASSIGNED) {
+		wcout << L"The Token does not have the specified privilege. " << endl;
+		return FALSE;
+	}
+	return TRUE;
+}
+
+BOOL EjectDll(DWORD dwPID, LPCTSTR szDllName) {
+	BOOL bMore = FALSE, bFound = FALSE;
+	HANDLE hSnapshot, hProcess, hThread;
+	HMODULE hModule = NULL;
+	MODULEENTRY32 me = { sizeof(me) };
+	LPTHREAD_START_ROUTINE pThreadProc;
+	hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
+
+	bMore = Module32First(hSnapshot, &me);
+
+	for (; bMore; bMore = Module32Next(hSnapshot, &me)) {
+		if (!_tcsicmp(me.szModule, szDllName) || !_tcsicmp(me.szExePath, szDllName)) {
+			bFound = TRUE;
+			break;
+		}
+	}
+
+	if (!bFound) {
+		CloseHandle(hSnapshot);
+		return FALSE;
+	}
+
+	if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwPID))) {
+		wcout << L"OpenProcess(" << dwPID << L") failed!!! [" << GetLastError() << L"]" << endl;
+		return FALSE;
+	}
+
+	hModule = GetModuleHandle(L"kernel32.dll");
+	pThreadProc = reinterpret_cast<LPTHREAD_START_ROUTINE>(GetProcAddress(hModule, "FreeLibrary"));
+
+	hThread = CreateRemoteThread(hProcess, NULL, NULL, pThreadProc, me.modBaseAddr, NULL, NULL);
+	WaitForSingleObject(hThread, INFINITE);
+
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+	CloseHandle(hSnapshot);
+
+	return TRUE;
+}
+int _tmain(int argc, TCHAR *argv)
+{
+	wcout.imbue(locale("kor"));
+	vector<DWORD> vc;
+	WCHAR name[50] = { NULL };
+	wcout << L"want Ejection DLL NAME:";
+	wcin >> name;
+	if (name[0] == NULL)
+		return NULL;
+	vc = FindProcessId(name);
+	if (vc.empty()) {
+		wcout << L"There is no " << name << L" process" << endl;
+		return NULL;
+	}
+
+	for (DWORD dwPID : vc) {
+		wcout << L"PID of " << name << L" is " << dwPID << endl;
+	}
+
+	if (!SetPrivilege(SE_DEBUG_NAME, TRUE)) {
+		return NULL;
+	}
+
+	for (DWORD dwPID : vc) {
+		if (EjectDll(dwPID, name)) {
+			wcout << L"EjectDll(" << dwPID << L", " << name << L" success!!" << endl;
+		}
+		else {
+			wcout << L"EjectDll(" << dwPID << L", " << name << L") failed!!" << endl;
+		}
+	}
+
+	return 1;
+
+}
